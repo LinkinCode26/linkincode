9eed704f5ae78c0a6a90af483a456b65a97962c6
Agustin <jorgeagustinaparicio@gmail.com>
2026-09-02 19:08:17 -0300

    feat: add navbar to website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import LanguageDropdown from "./components/LanguageDropdown";
 import ThemeToggle from "./components/ThemeToggle";
-import { Hero } from './sections/Hero'
+import { Hero } from './sections/Hero';
+import  Navbar  from './components/Navbar';
 
 function App() {
   return (
     <main className="relative min-h-screen bg-bg text-ink font-sans">
       <div className="fixed top-6 right-6 z-50 flex items-center gap-3">
+        <Navbar/>
         <ThemeToggle />
         <LanguageDropdown />
       </div>
