cb8bc134eac7c76d2b73ee56c205ee9a191760c3
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2026-09-02 22:51:34 -0300

    modificaciones t

--- client/src/sections/Footer.jsx
+++ client/src/sections/Footer.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
-import useLanguage from "../hooks/useLanguage";
 
 export default function Footer() {
-  const { t } = useLanguage();
   const [showBackToTop, setShowBackToTop] = useState(false);
 
   useEffect(() => {
