678e86eacb784a565c0c1955736eb1a97e944a17
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2026-09-14 20:46:31 -0300

    Creacion del User.js

---
+++ api/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// HOOK: Hashear la contraseña antes de guardar en la base de datos (Criterio 1)
+userSchema.pre("save", async function (next) {
+  // Si la contraseña no se modificó, pasamos de largo
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// MÉTODO: Comparar la contraseña ingresada con la hasheada
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
