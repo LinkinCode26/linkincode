70220e57ba531d2de31bc0bcc95dc072ae3e532f
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2026-09-23 11:20:52 -0300

    fix(leads.service): getFilteredLeads

--- api/services/leads.service.js
+++ api/services/leads.service.js
@@ -2,6 +2,15 @@ import Lead from "../models/Lead.js";
 
 export const createLead = async (data) => {
   const lead = await Lead.create(data);
-
   return lead;
+};
+
+export const getFilteredLeads = async (queryFilters, skip, limit) => {
+  const leads = await Lead.find(queryFilters)
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Lead.countDocuments(queryFilters);
+  return { leads, total };
 };
